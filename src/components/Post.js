43aa8863ff187f45957c27432c1173852cd5fc93
valentinbueno <vbueno@udesa.edu.ar>
2023-11-18 21:02:26 -0300

    avamces

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import react, { Component } from 'react';
-import {TouchableOpacity, View, Text, StyleSheet,Image} from 'react-native';
+import {TouchableOpacity, View, Text, StyleSheet,Image,TextInput} from 'react-native';
 import {db, auth } from '../firebase/config';
 import firebase from 'firebase';
 
@@ -10,7 +10,7 @@ class Post extends Component {
         super(props)
         this.state={
             like: false,
-
+            comment: [],
         }
 
 
@@ -42,8 +42,24 @@ class Post extends Component {
         })
     })
     .catch( e => console.log(e))
+   }
 
-
+   comment(comments, date){
+    let comment = {
+        userName: auth.currentUser.email,
+        createdAt:date,
+        texto: comments
+    }
+    db.collection('posts').doc(this.props.datapost.id).update({
+        comments: firebase.firestore.FieldValue.arrayUnion(comment)
+    })
+    .then(res => this.setState({
+        user: auth.currentUser.email,
+        comment: this.props.dataPost.datos.comment,
+        cantidadComments: this.props.dataPost.datos.comments.length
+    })
+    )
+    .catch(e => console.log(e))
    }
 
    dislike(){
@@ -74,37 +90,49 @@ class Post extends Component {
         console.log(this.props);
         return(
             <View style={styles.contenedor}>
-                <Text style={styles.data}>Usuario: {this.props.dataPost.datos.owner}</Text>
-                <Image style={styles.camera} source={{uri:this.props.dataPost.datos.photo }}/>
-                <Text style={styles.data}>Titulo: {this.props.dataPost.datos.post}</Text>
-
-                <Text>Cantidad de Likes: {this.props.dataPost.datos.likes.length}</Text>
-
-
+                <View style={styles.posteo}>
+                    <Text style={styles.data}>Usuario: {this.props.dataPost.datos.owner}</Text>
+                    <Image style={styles.camera} source={{uri:this.props.dataPost.datos.photo }}/>
+                    <Text style={styles.data}>Titulo: {this.props.dataPost.datos.post}</Text>
+                        <Text>Cantidad de Likes: {this.props.dataPost.datos.likes.length}</Text>
 
 
-               {/* If ternario */}
-               {this.state.like ? 
-                <TouchableOpacity style={styles.button2} onPress={()=>this.dislike()}>
-                    <Text style={styles.textButton} >Dislike</Text>
 
-                    
 
-                </TouchableOpacity>
-                :
-                <TouchableOpacity style={styles.button} onPress={()=>this.likear()}>
-                    <Text style={styles.textButton} >Like</Text>
-                </TouchableOpacity>
-                }
+                    {/* If ternario */}
+                    {this.state.like ? 
+                        <TouchableOpacity style={styles.button2} onPress={()=>this.dislike()}>
+                            <Text style={styles.textButton} >Dislike</Text>
 
-            {auth.currentUser.email == this.props.dataPost.datos.owner && 
-                        <TouchableOpacity style={styles.deletebutton} onPress={()=>this.deletePost()} activeOpacity={0.7}>
-                            <Text style={styles.textButton}>Delete post</Text>
+                            
+
                         </TouchableOpacity>
-                        } 
+                        :
+                        <TouchableOpacity style={styles.button} onPress={()=>this.likear()}>
+                            <Text style={styles.textButton} >Like</Text>
+                        </TouchableOpacity>
+                        }
 
-
-
+                    {auth.currentUser.email == this.props.dataPost.datos.owner && 
+                                <TouchableOpacity style={styles.deletebutton} onPress={()=>this.deletePost()} activeOpacity={0.7}>
+                                    <Text style={styles.textButton}>Delete post</Text>
+                                </TouchableOpacity>
+                                }
+                </View>
+                <View style={styles.comentarios}>
+                    <View style={styles.insertarComentario}>
+                        <TextInput
+                            style={styles.comentar}
+                            onChangeText={(text)=>this.setState({comment: text})}
+                            placeholder='Comentar...'
+                            keyboardType='default'
+                            value={this.state.comment}
+                        />
+                        {this.state.comment.length === 0 ?(<Text></Text>):(<TouchableOpacity style={styles.buttonComentar} onPress={()=>this.comment(this.state.text,Date.now())}>
+                            <Text style={styles.textButton}>Enviar</Text>    
+                        </TouchableOpacity>)}
+                    </View>
+                </View>
             </View>
         )
     }
@@ -115,12 +143,12 @@ const styles = StyleSheet.create({
       backgroundColor: "#28a745",
       paddingHorizontal: 10,
       paddingVertical: 6,
-      textAlign: "center",
+      textAlign:'center',
       borderRadius: 4,
       borderWidth: 1,
       borderStyle: "solid",
       borderColor: "#28a745",
-      width: "30%",
+      width:125,
       marginBottom: 5,
     },
 
@@ -128,12 +156,12 @@ const styles = StyleSheet.create({
         backgroundColor: "red",
         paddingHorizontal: 10,
         paddingVertical: 6,
-        textAlign: "center",
+        textAlign:'center',
         borderRadius: 4,
         borderWidth: 1,
         borderStyle: "solid",
         borderColor: "red",
-        width: "30%",
+        width:125,
         marginBottom: 5,
       },
 
@@ -146,13 +174,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: "solid",
         borderColor: "orange",
-        width: "30%",
+        width:125,
         marginBottom: 5,
       },
+
+    posteo:{
+        flexDirection:'column',
+        alignContent:'center',
+        justifyContent:'center',
+        marginLeft:50,
+    },
 
     camera: {
         height: 220,
         width: 300,
+        border:'solid',
+        borderSolid:'solid',
+        borderRadius:4,
+        borderWidth:1,
+        borderColor:'white',
     },
 
     textButton: {
@@ -167,6 +207,8 @@ const styles = StyleSheet.create({
         fontFamily:"Arial"
     },
     contenedor:{
+        flexDirection:'row',
+        backgroundColor:'#ff8370',
         borderStyle: 'solid',
         borderBlockColor:"black",
         borderWidth: 1,
@@ -176,10 +218,40 @@ const styles = StyleSheet.create({
         alignItems:'center',
         width: "100%",
         height: "auto"
-        
-        
     },
-
+    comentarios:{
+        marginLeft:40,
+    },
+    comentar:{
+        height:20,
+        width:300,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        backgroundColor:'white',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    insertarComentario:{
+        flexDirection:'row',
+        alignContent:'center',
+    },
+    buttonComentar:{
+        backgroundColor:'black',
+        padding:10,
+        justifyContent:'center',
+        alignContent:'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: 'black',
+        height:20,
+        width:60,
+        marginLeft:5,
+        marginTop:15,
+    },
     
   });
 
